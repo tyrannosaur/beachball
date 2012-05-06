a2e6bdc24ee3bcd1dcf94f12339dfb4688d716db
Charlie Liban <charlie@tyrannosaur.ca>
2012-05-06 16:47:21 -0400

    Testing

--- static/js/main.js
+++ static/js/main.js
@@ -59,15 +59,15 @@ var app = {};
 
       gx = [];
       gy = [];
-/*
+
       world.gravity({
          x : sumx,
          y : sumy
       });
-      */
+      
       var angle = Math.atan(sumy/(sumx + 0.00001));      
 
-      $('#counter').text(sumx + '<br/>' + sumy + '<br/>' + angle);
+      $('#counter').html(sumx + '<br/>' + sumy + '<br/>' + angle);
       /*
       bodies.beachball.ApplyImpulse({
          x : -Math.cos(angle) * settings.impulseMagnitude, 
